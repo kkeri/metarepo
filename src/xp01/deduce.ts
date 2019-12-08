ffc67d2b38147bceb016d8948d8642ba48869393
Kalman Keri <kk@metamaya.org>
2019-12-08 01:42:56 +0100

    xp01: clean up the deduce function and pass more test cases

--- src/xp01/deduce.ts
+++ src/xp01/deduce.ts
@@ -5,23 +5,26 @@ import { LogicalNormalForm, BinaryOp } from './types';
 export function makeDeduce ({ or, and, not }: LogicalNormalForm): BinaryOp {
 
   function deduce (a: Model, b: Model): Model {
+    // conjunction introduction
+    if (b instanceof And) return and(deduce(a, b.a), deduce(a, b.b))
+
+    // conjunction elimination
+    if (a instanceof And) return or(deduce(a.a, b), deduce(a.b, b))
+
+    // disjunction introduction
+    if (b instanceof Or) return or(deduce(a, b.a), deduce(a, b.b))
+
+    // disjunction elimination
+    if (a instanceof Or) return and(deduce(a.a, b), deduce(a.b, b))
+
     // identity
     if (equal(a, b)) return True
 
     // law of non-contradiction
     if (equal(not(a), b)) return False
 
-    // combinations
-    let result = False
-    // disjunction introduction
-    if (b instanceof Or) result = or(result, or(deduce(a, b.a), deduce(a, b.b)))
-    // disjunction elimination
-    if (a instanceof Or) result = or(result, and(deduce(a.a, b), deduce(a.b, b)))
-    // conjunction introduction
-    if (b instanceof And) result = or(result, and(deduce(a, b.a), deduce(a, b.b)))
-    // conjunction elimination
-    if (a instanceof And) result = or(result, or(deduce(a.a, b), deduce(a.b, b)))
-    return result !== False ? result : b
+    // none of the rules can be applied
+    return b
   }
 
   return deduce
