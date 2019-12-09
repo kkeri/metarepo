757d53e146560ea9e48256602fb81046650d6900
Kalman Keri <kk@metamaya.org>
2019-12-09 16:16:33 +0100

    xp01: add comment

--- src/xp01/interpreter.ts
+++ src/xp01/interpreter.ts
@@ -3,5 +3,7 @@ import { cnf } from './cnf';
 import { dnf } from './dnf';
 import { onf } from './onf';
 
-export const nf = onf
+// select normal form
+export const nf = dnf
+
 export const deduce = makeDeduce(nf)
